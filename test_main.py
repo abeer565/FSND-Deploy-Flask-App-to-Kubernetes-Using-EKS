a3cffe9b26f74a1d9a0375181d00f600d3c30076
abeer <53477589+abeer565@users.noreply.github.com>
2021-09-06 23:13:10 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -39,10 +39,4 @@ def test_auth(client):
     token = response.json['token']
     assert token is not None
 
- # test function 
- def test_negative(self):
-        testValue = True
-        # error message in case if test case got failed
-        message = "Test value is not false."
-        # assetFalse() to check test value as false
-        self.assertFalse( testValue, message)    
+
